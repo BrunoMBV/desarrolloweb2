13f7af44c1d3852e795b6ea14e6dd5d8d07f352b
BrunoMBV <166830063+BrunoMBV@users.noreply.github.com>
2024-04-17 09:31:23 -0400

    Add files via upload

---
+++ desarrolloweb2/js/variables.js
@@ -0,0 +1,19 @@
+let varb=true;//booleano
+let varn=3.141592;//numerico
+let vars="texto string";//cadena
+let var1;//indefinido
+
+console.log("para la variable varb"+varb+" con el tipo dato="+typeof varb);
+console.log("para la variable varn"+varb+" con el tipo dato="+typeof varn);
+console.log("para la variable vars"+varb+" con el tipo dato="+typeof vars);
+console.log("para la variable var1"+var1+" con el tipo dato="+typeof var1);
+
+//realizar las siguientes conversiones implicitas
+let var1=15.25;
+let var2=0
+let var3=true;
+let var4="100";
+let varx;
+varx=var1
+varx=var3;//colocar comentario
+console.log("para la variable var1="+varx+" con el tipo dato ")
